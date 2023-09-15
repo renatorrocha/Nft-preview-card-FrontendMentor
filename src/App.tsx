8e06aadd4b0b47d50e03e52725e60b01f228e97d
renatorrocha <renatorrodrigues2002@gmail.com>
2023-09-15 14:02:16 -0300

    esqueleto da aplicacao montado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
-
-import './App.css'
-
 function App() {
   return (
-    <>
-      <p className="text-red-600">ola mundo</p>
-    </>
+    <main className="min-h-screen bg-veryDarkBlue_main flex">
+      <div className="bg-veryDarkBlue_card flex flex-col">
+        <img
+          src="src/assets/images/image-equilibrium.jpg"
+          alt=""
+          className="h-64 w-64"
+        />
+
+        <div>
+          <p className="text-white text-lg font-bold">Equilibrium #3429</p>
+        </div>
+
+        <p className="text-softBlue text-sm">
+          Our Equilibrium collection promotes balance and calm.
+        </p>
+
+        <div className="flex items-center">
+          <img
+            src="src/assets/images/icon-ethereum.svg"
+            alt=""
+            className="h-4 w-2"
+          />
+          <p className="text-cyan">0.041 ETH</p>
+
+          <div className="flex items-center">
+            <img
+              src="src/assets/images/icon-clock.svg"
+              alt=""
+              className="h-4 w-4"
+            />
+            <p className="text-softBlue font-semibold">3 days left</p>
+          </div>
+        </div>
+        <hr />
+
+        <div className="flex items-center">
+          <img
+            src="src/assets/images/image-avatar.png"
+            alt=""
+            className="h-8 w-8"
+          />
+          <p className="text-softBlue font-medium">
+            Creation of <b className="text-white">Jules Wyvern</b>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        softBlue: 'hsl(215, 51%, 70%)',
+        cyan: 'hsl(178, 100%, 50%)',
+        veryDarkBlue_main: 'hsl(217, 54%, 11%)',
+        veryDarkBlue_card: 'hsl(216, 50%, 16%)',
+        veryDarkBlue_line: 'hsl(215, 32%, 27%)'
+      }
+    }
   },
   plugins: []
 }
