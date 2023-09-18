5f36cd93d503801891985205884d47bb8cc77ad7
renatorrocha <renatorrodrigues2002@gmail.com>
2023-09-18 09:33:31 -0300

    estilização finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
 function App() {
   return (
-    <main className="min-h-screen bg-veryDarkBlue_main flex items-center justify-center w-screen">
-      <div className="bg-veryDarkBlue_card rounded-2xl w-72 h-auto p-6">
-        <img
-          src="src/assets/images/image-equilibrium.jpg"
-          alt=""
-          className="h-60 w-60 rounded-xl"
-        />
+    <main className="flex min-h-screen w-screen flex-col items-center justify-center bg-veryDarkBlue_main">
+      <div className="h-auto w-72 rounded-2xl bg-veryDarkBlue_card p-6">
+        <div className="relative flex items-center justify-center overflow-hidden rounded-xl">
+          <img
+            src="src/assets/images/image-equilibrium.jpg"
+            alt=""
+            className="h-60 w-60 rounded-xl"
+          />
+          <div className="absolute bottom-0 left-0 right-0 top-0 flex cursor-pointer items-center justify-center overflow-hidden bg-cyan bg-opacity-20 bg-fixed opacity-0 transition-all duration-300 hover:opacity-100">
+            <img src="src/assets/images/icon-view.svg" alt="" />
+          </div>
+        </div>
 
-        <p className="text-white text-lg font-bold my-2">Equilibrium #3429</p>
+        <p className="font-outift my-2 text-lg font-bold text-white transition-all duration-300 ease-in-out hover:cursor-pointer hover:text-cyan">
+          Equilibrium #3429
+        </p>
 
-        <p className="text-softBlue text-sm">
+        <p className="font-outift text-sm text-softBlue">
           Our Equilibrium collection promotes balance and calm.
         </p>
 
-        <div className="flex justify-between item">
-          <div className="flex items-center my-2">
+        <div className="item flex justify-between">
+          <div className="my-2 flex items-center">
             <img
               src="src/assets/images/icon-ethereum.svg"
               alt=""
-              className="h-3 w-2 mr-1"
+              className="mr-1 h-3 w-2"
             />
-            <p className="text-cyan font-semibold text-sm">0.041 ETH</p>
+            <p className="font-outift text-sm font-semibold text-cyan">
+              0.041 ETH
+            </p>
           </div>
 
           <div className="flex items-center">
@@ -30,25 +39,42 @@ function App() {
               alt=""
               className="h-4 w-4"
             />
-            <p className="text-softBlue font-semibold text-sm">3 days left</p>
+            <p className="font-outift text-sm font-semibold text-softBlue">
+              3 days left
+            </p>
           </div>
         </div>
 
         <hr className="border-veryDarkBlue_line" />
 
-        <div className="flex items-center mt-3">
+        <div className="mt-3 flex items-center">
           <img
             src="src/assets/images/image-avatar.png"
             alt=""
-            className="h-6 w-6 border border-white rounded-full"
+            className="h-6 w-6 rounded-full border border-white"
           />
-          <p className="text-softBlue font-medium text-sm ms-3">
-            Creation of <b className="text-white">Jules Wyvern</b>
+          <p className="font-outift ms-3 text-sm font-medium text-softBlue">
+            Creation of{" "}
+            <b className="font-outift text-white transition-all duration-300 ease-in-out hover:cursor-pointer hover:text-cyan">
+              Jules Wyvern
+            </b>
           </p>
         </div>
       </div>
+      <footer className="pt-6">
+        <p className="font-outift text-gray-400">
+          Feito por{" "}
+          <a
+            href="https://github.com/renatorrocha"
+            target="_blank"
+            className="transition-all duration-300 ease-in-out hover:text-gray-200"
+          >
+            @renatorrodrigues
+          </a>
+        </p>
+      </footer>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        softBlue: 'hsl(215, 51%, 70%)',
-        cyan: 'hsl(178, 100%, 50%)',
-        veryDarkBlue_main: 'hsl(217, 54%, 11%)',
-        veryDarkBlue_card: 'hsl(216, 50%, 16%)',
-        veryDarkBlue_line: 'hsl(215, 32%, 27%)'
-      }
-    }
+        softBlue: "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
+        veryDarkBlue_main: "hsl(217, 54%, 11%)",
+        veryDarkBlue_card: "hsl(216, 50%, 16%)",
+        veryDarkBlue_line: "hsl(215, 32%, 27%)",
+      },
+      fontFamily: {
+        outift: ["Outfit", "sans-serif"],
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
