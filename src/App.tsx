aca6a98736a6fd780541e21f67fc3f14b100db28
renatorrocha <renatorrodrigues2002@gmail.com>
2023-09-18 08:38:06 -0300

    adicionando espacamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 function App() {
   return (
-    <main className="min-h-screen bg-veryDarkBlue_main flex">
-      <div className="bg-veryDarkBlue_card flex flex-col">
+    <main className="min-h-screen bg-veryDarkBlue_main flex items-center justify-center w-screen">
+      <div className="bg-veryDarkBlue_card rounded-2xl w-72 h-auto p-6">
         <img
           src="src/assets/images/image-equilibrium.jpg"
           alt=""
-          className="h-64 w-64"
+          className="h-60 w-60 rounded-xl"
         />
 
-        <div>
-          <p className="text-white text-lg font-bold">Equilibrium #3429</p>
-        </div>
+        <p className="text-white text-lg font-bold my-2">Equilibrium #3429</p>
 
         <p className="text-softBlue text-sm">
           Our Equilibrium collection promotes balance and calm.
         </p>
 
-        <div className="flex items-center">
-          <img
-            src="src/assets/images/icon-ethereum.svg"
-            alt=""
-            className="h-4 w-2"
-          />
-          <p className="text-cyan">0.041 ETH</p>
+        <div className="flex justify-between item">
+          <div className="flex items-center my-2">
+            <img
+              src="src/assets/images/icon-ethereum.svg"
+              alt=""
+              className="h-3 w-2 mr-1"
+            />
+            <p className="text-cyan font-semibold text-sm">0.041 ETH</p>
+          </div>
 
           <div className="flex items-center">
             <img
@@ -30,18 +30,19 @@ function App() {
               alt=""
               className="h-4 w-4"
             />
-            <p className="text-softBlue font-semibold">3 days left</p>
+            <p className="text-softBlue font-semibold text-sm">3 days left</p>
           </div>
         </div>
-        <hr />
 
-        <div className="flex items-center">
+        <hr className="border-veryDarkBlue_line" />
+
+        <div className="flex items-center mt-3">
           <img
             src="src/assets/images/image-avatar.png"
             alt=""
-            className="h-8 w-8"
+            className="h-6 w-6 border border-white rounded-full"
           />
-          <p className="text-softBlue font-medium">
+          <p className="text-softBlue font-medium text-sm ms-3">
             Creation of <b className="text-white">Jules Wyvern</b>
           </p>
         </div>
